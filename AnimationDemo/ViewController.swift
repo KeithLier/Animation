a5ad6a3cc699d513729cbc800247816eae8f4901
Keith <li.liang@lifecycle.cn>
2016-08-26 17:20:41 +0800

    Initial Commit

---
+++ AnimationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Keith on 16/8/26.
+//  Copyright © 2016年 Keith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
